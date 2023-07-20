2475c635cdda83cda316ebd02ff59f4d91a9487c
rustandbone <dlehdgh14@naver.com>
2023-07-21 00:04:06 +0900

    [main js] 모듈화 import, 코드 설명, 타입 체크 추가, 함수 내 순서 변경

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -1,3 +1,5 @@
+import { addClass, attr, data, node, nodes } from "./index.js";
+
 /* 
 1. 클릭 이벤트 활성화
 2. nav 클릭시 배경 색상 변경
@@ -6,58 +8,87 @@
 5. 함수 분리
 */
 
+//클릭 이벤트 발생 시 실행 함수
 function handleSlider(e) {
   e.preventDefault();
 
   const target = e.target.closest("li");
   if (!target) return;
+  //선택한 요소의 인덱스 및 data 가져오기
+  const index = attr(target, "data-index");
+  const dataSet = data[index - 1];
 
+  //html 제어할 요소 가져오기
   const list = nodes(".character__li");
   const visualImage = node(".visual__img");
   const visualAudio = node(".visual__audio");
   const name = node(".nickName");
   const body = node(".posterBody");
 
-  const index = attr(target, "data-index");
-  const dataSet = data[index - 1];
-
+  //1. 클래스 삭제 및 선택한 요소에 is-active 클래스 추가
   classReset(list, "is-active");
-  target.classList.add("is-active");
+  addClass(target, "is-active");
 
+  //2. 배경색 설정
+  setBgcolor(body, dataSet.color);
+
+  //3. 메인 이미지, 오디오 설정
   setContent(visualImage, { src: dataSet.name, alt: dataSet.alt });
   setContent(visualAudio, { src: dataSet.name });
+
+  //4. 캐릭터 이름 설정
   setNameText(name, dataSet.name);
-  setBgcolor(body, dataSet.color);
 }
 
 node(".nav").addEventListener("click", handleSlider);
 
+//클래스 삭제 함수
 function classReset(node, className) {
+  if (typeof node === "string") {
+    node = nodes(node);
+  }
+
   node.forEach((item) => item.classList.remove(className));
 }
 
+//배경색 설정 함수
 function setBgcolor(node, color) {
-  if (!Array.isArray(color) || !color[1]) {
+  if (typeof node === "string") {
+    node = node(node);
+  }
+
+  if (!Array.isArray(color)) {
     node.style.background = `linear-gradient(to bottom, ${color}, #000)`;
   } else {
-    node.style.background = `linear-gradient(to bottom, ${color[0]}, ${color[1]})`;
+    const [colorA, colorB = "#000"] = color;
+    node.style.background = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
   }
 }
 
-function setContent(node, attr) {
-  const imgSrc = `./assets/${attr.src}.jpeg`;
-  const audioSrc = `./assets/audio/${attr.src}.m4a`;
-  const attrSrc = Object.keys(attr).find((key) => key === "src");
-  const attrAlt = Object.keys(attr).find((key) => key === "alt");
+//이미지, 오디오 변경 함수
+function setContent(node, value) {
+  if (typeof node === "string") {
+    node = node(node);
+  }
+
+  const imgSrc = `./assets/${value.src}.jpeg`;
+  const audioSrc = `./assets/audio/${value.src}.m4a`;
+  const attrSrc = Object.keys(value).find((key) => key === "src");
+  const attrAlt = Object.keys(value).find((key) => key === "alt");
 
   if (node.tagName === "IMG") {
-    node.setAttribute(attrAlt, attr.alt);
-    return node.setAttribute(attrSrc, imgSrc);
+    attr(node, attrAlt, value.alt);
+    return attr(node, attrSrc, imgSrc);
   } else if (node.tagName === "AUDIO") {
-    return node.setAttribute(attrSrc, audioSrc);
+    return attr(node, attrSrc, audioSrc);
   }
 }
 
+//텍스트 변경 함수
 function setNameText(node, value) {
-  node.textContent = value;
+  if (typeof node === "string") {
+    node = node(node);
+  }
+
+  node.textContent = `${value}`;
 }
